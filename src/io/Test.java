26d7fdc72aff9507cd1617b7cb5e3d7ca765e16b
kl3pt0x <kl3pt0x@gmail.com>
2018-11-22 10:40:04 +0100

    test2

---
+++ src/io/Test.java
@@ -0,0 +1,14 @@
+package io;
+
+/**
+ * Beschreibung der Klasse Test.
+ * Weiter Beschreibung
+ * <p>
+ * Die Klasse wurde am 22.November.2018 um 10:39 Uhr erstellt.
+ *
+ * @author Pascal Rausch
+ * @version 1.0
+ */
+
+public class Test {
+}
